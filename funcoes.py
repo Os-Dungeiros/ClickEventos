f7124932b13bf54c76434b4594c76cb0c1b02beb
Daniel Camargo <88405313+DanielRubioCamargo@users.noreply.github.com>
2021-10-19 10:31:23 -0300

    Modificação na função de cadastro de usuário!

--- funcoes.py
+++ funcoes.py
@@ -19,10 +19,21 @@ def criar_menu(lista,titulo:str,tamanhoLinha = 55):
 
 
 def cadastrar_usuario(lista,nome:str,email:str):
-    dadosAlunos = {"nome":nome,"email":email}
-    lista.append(dadosAlunos.copy())
-    dadosAlunos.clear()
-    print("\033[1;32mUsuário {} cadastrado(a) com sucesso!\033[m".format(nome))
+    jaTem = False
+    for i,c in enumerate(lista):
+        if c["email"] == email:
+            jaTem = False
+        elif i == len(lista) - 1:
+            jaTem = True
+    if jaTem == False:
+        dadosAlunos = {"nome":nome,"email":email}
+        lista.append(dadosAlunos.copy())
+        dadosAlunos.clear()
+        print("\033[1;32mUsuário {} cadastrado(a) com sucesso!\033[m".format(nome))
+    else:
+        print("\033[1;31mEsse endereço de email já existe! Erro ao cadastrar usuário!\033[m")
+    
+    
 def exibir_usuarios_cadastrados(lista):
     for i,c in enumerate(lista):
         print("{} --> {}".format(i+1,c["nome"].title()))
@@ -44,6 +55,8 @@ def verificar_usuario_nome(lista,nome:str):
             break
         elif i == len(lista) - 1:
             return False
+        
+        
 def verificar_usuario_email(lista,email:str):
     for i,c in enumerate(lista):
         if c["email"] == email:
@@ -51,6 +64,7 @@ def verificar_usuario_email(lista,email:str):
             break
         elif i == len(lista) - 1:
             return False
+        
 
 def remover_aluno(lista,email:str):
     for i,c in enumerate(lista):
@@ -61,6 +75,7 @@ def remover_aluno(lista,email:str):
                 print("\033[1;32mUsuário {} removido(a) da lista com sucesso!\033[m".format(c["nome"]))
             else:
                 print("\033[1;32mPedido de exclusão cancelado com sucesso!\033[m")
+                
 
 def alterar_nome_usuario(lista,email:str,novoNome:str):
     cont = 0
